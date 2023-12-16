2669259143d840042079c60589a728035305604d
ekimaycicek <ekimaycicek@icloud.com>
2023-12-16 21:31:52 +0300

    new

--- src/main/java/Method.java
+++ src/main/java/Method.java
@@ -95,6 +95,7 @@ public class Method {
 
 
 
+
     }
     public void result(){
         System.out.println("your coffee " + dimension +" "  + "size " +   whichCoffee + " prepared in the form.Enjoy your meal..." ) ;
