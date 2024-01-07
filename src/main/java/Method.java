57a8ff845c237956eb77d6fdbde12787bdad1502
ekimaycicek <ekimaycicek@icloud.com>
2024-01-07 19:17:56 +0300

    new

--- src/main/java/Method.java
+++ src/main/java/Method.java
@@ -79,6 +79,7 @@ public class Method {
 
 
 
+
     public void chooseSize(){
         System.out.println("What size would it be? (Enter large - medium - small.)");
         dimension=input.next().toLowerCase();
